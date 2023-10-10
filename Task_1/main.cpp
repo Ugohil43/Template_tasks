02dccaeee9afc1f954b10a60d57021af4711ec62
ezhov.iv <ezhoviv@npo-angstrem.ru>
2023-10-10 15:52:35 +0300

    A new version of Task_1 has been implemented.

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,107 +1,40 @@
 #include <iostream>
+#include <string>
 #include "MyVector.h"
 //cmake -G "MinGW Makefiles" ../
 
 int main()
 {
     std::cout << "Beginning of program... " << std::endl;
+
+    std::cout << "________Tes 1________" << std::endl;
+    MyVector<int> v1(10);
+    std::size_t n1 = v1.size();
+    for (int i = 0; i < n1; ++i)
+        v1[i] = i+1;
+    v1.show();
+    std::cout << std::endl;
+
+    std::cout << "________Tes 2________" << std::endl;
+    std::string initStr[5] = {"first","second","third","fourth","fifth"};
     
-    MyVector<int> V1(1,0);
-    MyVector<double> V2;
-    MyVector<char> V3;
-    char a = 11, b = 22, c = 33;
-    int type;
+    MyVector<std::string> v2(5);
+    std::size_t n2 = v2.size();
+    for (int i = 0; i < n2; ++i)
+        v2[i] = initStr[i];
 
-    for(int i = 1; i <= 9; i++)
-    {
-        V1.push_back(i);
-    }
+    v2.show();
+    v2.insert(v2.begin() + 3, "After_third");
+    v2.show();
+    //std::cout << v2[6] << std::endl;
+    v2.push_back("Add_1");
+    v2.push_back("Add_2");
+    v2.push_back("Add_3");
+    v2.show();
 
-    V2.push_back(2.5);
-    V2.push_back(3.14);
-    V2.push_back(56.325);
-    
-    V3.push_back(a);
-    V3.push_back(b);
-    V3.push_back(c);
-
-    std::cout << "Enter an number of type (0 - integer; 1 - double; 2 - char): ";
-    std::cin >> type;
-    switch (type)
-    {
-    case 0:
-    std::cout << "________Integer Numbers________"<< std::endl << std::endl;
-
-    std::cout << "Int vector : " << V1 << std::endl;
-    std::cout << "Element at index [3] : " << V1[3] << std::endl;
-    std::cout << "Element at index [7] : " << V1[7] << std::endl;
-    std::cout << "Length of vector : "  << V1.length()  << std::endl << std::endl;
-
-    std::cout << "Insert new values..." << std::endl << std::endl;
-    V1.insert(2,40);
-    V1.insert(4,60);
-    std::cout << "Int vector : " << V1 << std::endl;
-    std::cout << "Length of vector : "  << V1.length()  << std::endl << std::endl;
-
-    std::cout << "Push back new value..." << std::endl << std::endl;
-    V1.push_back(100);
-    std::cout << "Int vector : " << V1 << std::endl;
-    std::cout << "Length of vector : "  << V1.length()  << std::endl << std::endl;
- 
-    std::cout << "Delete back value..." << std::endl << std::endl;
-    V1.pop_back();
-    std::cout << "Int vector : " << V1 << std::endl;
-    std::cout << "Length of vector : "  << V1.length()  << std::endl << std::endl;
-        break;//case 0
-    case 1:
-    std::cout << "________Double Numbers________"<< std::endl << std::endl;
-    
-    std::cout << "Double vector : " << V2 << std::endl;
-    std::cout << "Element at index [1] : " << V2[1] << std::endl;
-    std::cout << "Element at index [2] : " << V2[2] << std::endl;
-    std::cout << "Length of vector : "  << V2.length()  << std::endl << std::endl;
-
-     std::cout << "Insert new values..." << std::endl << std::endl;
-    V2.insert(2,(double)(rand())/RAND_MAX);
-    std::cout << "Double vector : " << V2 << std::endl;
-    std::cout << "Length of vector : "  << V2.length()  << std::endl << std::endl;
-
-    std::cout << "Push back new value..." << std::endl << std::endl;
-    V2.push_back(0.746605);
-    std::cout << "Double vector : " << V2 << std::endl;
-    std::cout << "Length of vector : "  << V2.length()  << std::endl << std::endl;
- 
-    std::cout << "Delete back value..." << std::endl << std::endl;
-    V2.pop_back();
-    std::cout << "Double vector : " << V2 << std::endl;
-    std::cout << "Length of vector : "  << V2.length()  << std::endl << std::endl;
-        break;//case 1
-    case 2:
-    std::cout << "________Char type________"<< std::endl << std::endl;
-    
-    std::cout << "Char vector : " << V3 << std::endl;
-    std::cout << "Element at index [1] : " << V3[1] << std::endl;
-    std::cout << "Element at index [2] : " << V3[2] << std::endl;
-    std::cout << "Length of vector : "  << V3.length()  << std::endl << std::endl;
-
-    std::cout << "Insert new values..." << std::endl << std::endl;
-    V3.insert(2,a);
-    std::cout << "Char vector : " << V3 << std::endl;
-    std::cout << "Length of vector : "  << V3.length()  << std::endl << std::endl;
-
-    std::cout << "Push back new value..." << std::endl << std::endl;
-    V3.push_back(b);
-    std::cout << "Char vector : " << V3 << std::endl;
-    std::cout << "Length of vector : "  << V3.length()  << std::endl << std::endl;
- 
-    std::cout << "Delete back value..." << std::endl << std::endl;
-    V3.pop_back();
-    std::cout << "Char vector : " << V3 << std::endl;
-    std::cout << "Length of vector : "  << V3.length()  << std::endl << std::endl;
-        break;//case 2
-    default:
-        std::cout << "Incorrect value";
-        return 0;
-    }//switch (type) 
+    v2.pop_back();
+    v2.pop_back();
+    v2.show();
+    std::cout << std::endl;
     return 0;
 }
--- Task_1/src/MyVector.h
+++ Task_1/src/MyVector.h
@@ -5,224 +5,161 @@
 template <typename T>
 class MyVector
 {
-private:
-    std::size_t size;
-    std::size_t count; 
-    T *arr;             
-public:
-    MyVector();
-    MyVector(const std::size_t size, const T& value);
-    ~MyVector();
+    private:
+    T* first;                                    // указатель на первый элекмент
+    T* last;                                     // указатель на последний элемент
+    
+    void Allocate(std::size_t n)                 // выделение памяти
+    {
+        first = new T [n*sizeof(T)];   // выделение памяти
+        last = first + n;              // опеределение указателя на последний элемент
+    }
+    
+    void Destroy()                               // удаление вектора
+    {
+        for (T* p = first; p != last; ++p) p->~T();   // вызов декструктора для всех элементов вектора
+        delete [] first;                              // освобождение памяти
+    }
 
-    void push_back(const T& value);                         // добавить элемент в конец вектора
-    void pop_back();                                        // удалить элемент с конеца вектора
-    void insert(const std::size_t& index, const T& value);  // вставка значения по индексу 
-    //void reserve(const std::size_t& size);
+    public:
+    explicit MyVector() : first(0), last(0) {}    // конструктор по умолчанию
+    explicit MyVector(std::size_t n, const T& vec = T())
+    {
+        Allocate(n);
+        for (std::size_t i = 0; i < n; i++)
+        {
+            *(first + i) = vec;
+        }
+    }
 
-    inline std::size_t length() const;
+    MyVector(const MyVector&);                    // конструктор копирвоания
+    MyVector& operator=(const MyVector&);         // оператор присваивания
+    ~MyVector() 
+    {
+        Destroy();
+        first = 0, last = 0;
+    }
 
-    T& operator[](const std::size_t& index) const;          // перегрузка оператора индексирвоания
-    MyVector<T> operator=(const MyVector<T>& v);            // перегрузка оператора присваивания   
-    MyVector<T> operator=(MyVector<T>&& v);                 // перегрузка оператора присваивания с помощью универсальной ссылки
-
-    template<typename T1>
-    friend std::ostream& operator<<(std::ostream& out, const MyVector<T1>& v);
+    std::size_t size() const;                     // размер вектора
+    T* begin() const {return first;}              // указатель на первый элемент 
+    T* end() const {return last;}                 // указатель на элемент, следующий за последним 
+    
+    T& operator[](std::size_t i);                 // индексирование
+    void insert(T* pos, const T& elem);           // вставка элемента в заданную позицию 
+    void push_back(const T& elem);                // вставка элемента в конец вектора 
+    void pop_back();                              // удаление элемента из конца вектора
+    void show() const;                            // вывод содержимого вектора  
 };
 
+// конструктор копирвоания
 template <typename T>
-inline MyVector<T>::MyVector()
+inline MyVector<T>::MyVector(const MyVector& other)
 {
-    this->size = 0;
-    this->count = 1;
-    arr = new T[1];
-     for (std::size_t i = 0; i < size; i++) 
+    std::size_t n = other.size();
+    Allocate(n);
+    for (std::size_t i = 0 ; i < n; ++i)
     {
-      this->arr[i] = NULL;     // заполняем
-    }
+        *(first + i) = *(other.first + i);
+    };
 }
 
+// оператор присваивания
 template <typename T>
-inline MyVector<T>::MyVector(const std::size_t size, const T& value)
+inline MyVector<T>& MyVector<T>::operator =(const MyVector& other)
 {
-    this->size = size;
-    this->count = size;
-    arr = new T[size];          // выделяем память
-    for (std::size_t i = 0; i < size; i++) 
+    if (this == &other) return *this;
+    Destroy();
+    std::size_t n = other.Size();
+    Allocate(n);
+
+    for (std::size_t i = 0 ; i < n; ++i)
     {
-      this->arr[i] = value;     // заполняем
-    }
+        *(first + i) = *(other.first + i);
+    };
+    return *this;
 }
 
+// размер вектора
 template <typename T>
-inline MyVector<T>::~MyVector()
+inline std::size_t MyVector<T>::size() const
 {
-    delete[] arr;
-    arr = nullptr;
+    return (0 == first ? 0 : last - first);
 }
 
+// индексирование
 template <typename T>
-inline void MyVector<T>::push_back(const T& value)
+inline T &MyVector<T>::operator[](std::size_t i)
 {
-    /*
-    if(count == size)
+    if (i < 0 || i > (size() - 1))
     {
-        reserve(size);
+        std::cout << "Incorrect index! " << std::endl;
     }
-    arr[count++] = value;
-    */
-    if (this->size == this->count)          // если массив заполнен
-    {        
-      this->count *= 2;                     // увеличиваем его в 2 раза
-      T* new_arr = new T[this->count];      // создаем новый массив
-      for (std::size_t i = 0; i < this->size; i++) 
-      {
-        new_arr[i] = this->arr[i];          // копируем данные
-      }
-      delete[] this->arr;                   // удаляем старый массив
-      this->arr = new_arr;                  // меняем указатель
-    }
-    this->arr[this->size] = value;          // добавляем элемент
-    this->size++;                           // увеличиваем размер
+    else
+    {
+        return (*(first + i));
+    };
 }
 
+// вставка элемента в заданную позицию
+template <typename T>
+inline void MyVector<T>::insert(T *pos, const T &elem)
+{
+    std::size_t n = size() + 1;             // новый размер
+    T* new_first = new T [n * sizeof(T)];   
+    T* new_last = new_first + n;
+
+    std::size_t offset = pos - first; // смещение соответсвующее адресу pos
+    for (std::size_t i = 0; i < offset; ++i) // копирование в новый вектор первой части
+    {
+        *(new_first + i) = *(first + i);
+    };
+
+    *(new_first + offset) = elem;
+
+
+    for (std::size_t i = offset; i < n; ++i) // копирование второй части
+    {
+        *(new_first + i + 1) = *(first + i);
+    }
+    Destroy();
+    first = new_first;
+    last = new_last;
+}
+
+// вставка элемента в конец вектора
+template <typename T>
+inline void MyVector<T>::push_back(const T &elem)
+{
+    if (!size())   // если вектор пууст 
+    {
+        Allocate(1);
+        *first = elem;
+    }
+    else
+    {
+        insert(end(), elem);
+    }
+}
+
+// удаление элемента из конца вектора
 template <typename T>
 inline void MyVector<T>::pop_back()
 {
-    if(!this->size)
-    {
-        return; 
-    }
-    this->size--;
+    T* p = end() - 1; // указатель на последний элемент вектора 
+    p->~T(); // удаляем элемент 
+    last--; 
 }
 
+// вывод содержимого вектора
 template <typename T>
-inline void MyVector<T>::insert(const std::size_t& index, const T& value)
+inline void MyVector<T>::show() const
 {
-    /*
-    if(index >= count || index < 0)
+    std::size_t n = size();
+    for (std::size_t i = 0 ; i < n; ++i)
     {
-        throw "Incorrect index!";
-    }
-    
-    if(count == size)
-    {
-        //reserve(size);
-        size*2;
-    }
-    
-    for(std::size_t i = count - 1; i >= index; i--)
-    {
-        arr[i + 1] = arr[i];
-    }
-
-    arr[index] = value;
-    count++;
-    */
-    if (index >= this->size)                             // если индекс больше размера массива
-    {                              
-      throw std::out_of_range("Incorrect index!");      // бросаем исключение
-    }
-    if (this->size == this->count)                      // если массив заполнен
-    {  
-      this->count *= 2;                                 // увеличиваем его в 2 раза
-      T* new_arr = new T[this->count];                  // создаем новый массив
-      for (std::size_t j = 0; j < this->size; j++) 
-      {
-        new_arr[j] = this->arr[j];                       // копируем данные
-      }
-      delete[] this->arr;                               // удаляем старый массив
-      this->arr = new_arr;                              // меняем указатель
-    }
-    for (std::size_t j = this->size; j > index; j--) 
-    {
-      this->arr[j] = this->arr[j - 1];                  // сдвигаем элементы
-    }
-    this->arr[index] = value;                            // вставляем элемент
-    this->size++;                                       // увеличиваем размер массива
-}
-
-/*
-template <typename T>
-inline void MyVector<T>::reserve(const std::size_t& size)
-{
-    if(size <= 0)
-    {
-        return;
-    }
-    std::cout << "Size: " << this->size << std::endl;
-    this->size = size*2;
-    std::cout << "Size: " << this->size << std::endl;
-    std::cout << "sizeof(T): " << sizeof(T) << std::endl;
-    T *new_arr = new T[size];
-    std::cout << "sizeof(T): " << sizeof(T) << std::endl;
-    memmove(new_arr, arr, sizeof(T) * count);
-    
-    delete arr;
-    arr = new_arr;
-}
-*/
-
-template <typename T>
-inline std::size_t MyVector<T>::length() const
-{
-     return size;
-}
-
-template <typename T>
-inline T &MyVector<T>::operator[](const std::size_t& index) const
-{
-    if(index >= count)
-    {
-        throw "Index out of range!";
-    }
-    
-    return this->arr[index];
-}
-
-template <typename T>
-inline MyVector<T> MyVector<T>::operator=(const MyVector<T>& v)
-{
-    if(this == &v)          // проверка на самокопирование
-    {
-        return *this;
-    }
-    
-    size  = v.size;
-    count = v.count;
-    
-    delete[] arr;
-    arr = new T[size];
-    memmove(arr, v.arr, count * sizeof(T));
-    
-    return *this;
-}
-
-template <typename T>
-inline MyVector<T> MyVector<T>::operator=(MyVector<T>&& v)
-{   
-    if(this == &v)          // проверка на самокопирование
-    {
-        return *this;
-    }
-    
-    size  = v.size;
-    count = v.count;
-    arr   = v.arr;
-    
-    v.arr = nullptr;
-    
-    return *this;
-}
-
-template <typename T1>
-inline std::ostream &operator<<(std::ostream& out, const MyVector<T1>& v)
-{
-    for(std::size_t i = 0; i < v.size; i++)
-    {
-        out << v[i] << " ";
-    }
-    
-    return out;
+        std::cout << *(first + i) << " ";
+    };
+    std::cout << std::endl;
 }
 
 #endif
